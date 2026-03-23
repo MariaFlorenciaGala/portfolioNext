4ac6cc9722e8aa11bcccb729194a1296dd453a30
MariaFlorenciaGala <mariaflorenciagala8@gmail.com>
2026-03-23 11:43:35 -0300

    I've added the reusable container

---
+++ landing_particles/components/container.tsx
@@ -0,0 +1,19 @@
+/**Container components
+ * Aplica las props de los estilos para no repetilos en cada pagina
+ */
+
+interface ContainerProps {
+    children:React.ReactNode
+}
+
+const ContainerPage = (props:ContainerProps) => {
+    const { children } = props
+
+    return(
+        <div className="w-full max-w-6xl px-4 pb-40 mx-auto mt-40 md:pb-0 md:px-6">
+            {children}
+        </div>
+    )
+}
+
+export default ContainerPage
